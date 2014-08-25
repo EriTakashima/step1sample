182767e318f30f1873cf90c3bc9f146491f3f17a
EriTakashima <tksmeri@gmail.com>
2014-08-26 01:01:33 +0900

    8/25までの進捗分
    ストレージ部分が完成．
    今後テーブルで出力する部分を作成する．

--- todo.js
+++ todo.js
@@ -6,7 +6,8 @@ function loaded(){
   $("#formButton").click(
     // コールバックとしてメソッドを引数にわたす
     function() {
-	  $("#ex").children.remove();
+	 $("#ex").text("");
+	 $("#warning").text("");
       saveText();
       showText();
     });
@@ -14,14 +15,23 @@ function loaded(){
 }
 // 入力された内容をローカルストレージに保存する
 function saveText() {
-  // 時刻をキーにして入力されたテキストを保存する
-  var text = $("#formText");
-  var time = new Date();
-  var limit = $("#formLimit");
-  localStorage.setItem(time, text.val(),limit.val());
-  // テキストボックスを空にする
-  text.val("");
-  limit.val("");
+	  // 時刻をキーにして入力されたテキストを保存する
+	 var t = $("#formText");
+	 var l = $("#formLimit");
+	 var time = new Date();
+	 var data = {
+		 text : escapeText(t.val()) ,
+		 limit : escapeText(l.val()) }
+
+	 if(l.val().match(/^[0-9][0-9][0-9][0-9]\/[0-9][0-9]\/[[0-9][0-9]$/)){
+		 localStorage.setItem(time, JSON.stringify(data));
+		 // テキストボックスを空にする
+		 t.val("");
+		 l.val("");
+	 }
+	 else{
+		 $("#warning").text("yyyy/mm/ddの形式にしたがって入力してください");
+	 }
 }
 
 // ローカルストレージに保存した値を再描画する
@@ -33,15 +43,26 @@ function showText() {
   var key, value, html = [];
   for(var i=0, len=localStorage.length; i<len; i++) {
     key = localStorage.key(i);
-    value = localStorage.getItem(key);
-    html.push("<div id=\"list\" class=\"box\">"+"<h2>"+ value +"</h2>"+"</div>");
-   //html.push("<div id=\"list\" class=\"box\">"+"<h2>"+ value +"</h2>"+"<p>"+"期限"+value+"</p>"+"<p>"+"作成"+time+"</p>"+"</div>");
+    value = JSON.parse(localStorage.getItem(key));
+    //html.push("<div id=\"list\" class=\"box\">"+"<h2>"+ key+":text " + value.text+" limit "+value.limit+"</h2>"+"</div>");
+	html.push("<div id=\"list\" class=\"box\">
+		 <table>
+			 <tr><td colspan=\"3\">新しいToDoをつくる</td></tr>
+			 <tr><td id=\"right\">ToDo名</td><td>"+ value.limit +"</td><td rowspan="2"> <input id="formButton" type="button" value="ToDoの追加"></td></tr>
+			 <tr><td id="right">期限 </td><td><input id="formLimit" type="text" size="40" maxlength="30" placeholder="yyyy/mm/dd"></td></tr>
+			 <tr><td colspan="3"><div class = "red-bolder" id="warning"></div></td></tr>
+		 </table>
+		 </div>"
+	 );
   }
   list.append(html.join(''));
 }
 function change_text(){
-console.log("aaaa");
- $("#ex").text("ToDoはありません。ToDoを追加してください");
+//バルス！
 localStorage.clear();
-console.log("bbbb");
+showText();
+ $("#ex").text("ToDoはありません。ToDoを追加してください");
+}
+function escapeText(text) {
+  return $("<div>").text(text).html();
 }
