6df1d3e6f8743421ff18f4aa4849ead7aa091465
EriTakashima <tksmeri@gmail.com>
2014-08-26 15:09:42 +0900

    15:00までの進捗分
    フラグが動くことを確認．

--- todo.js
+++ todo.js
@@ -1,17 +1,19 @@
 ﻿$(loaded);
 function loaded(){
 // console.log(now);
-   showText();
+	  if(showText() == false){
+		  $("#ex").text("ToDoはありません。ToDoを追加してください");
+		 }
   // ボタンをクリックしたときに実行するイベントを設定する
   $("#formButton").click(
     // コールバックとしてメソッドを引数にわたす
     function() {
 	 $("#ex").text("");
 	 $("#warning").text("");
-      saveText();
-      showText();
+     saveText();
+     showText();
     });
-	$("button").click(change_text);
+	$("button").click(clear_text);
 }
 // 入力された内容をローカルストレージに保存する
 function saveText() {
@@ -23,7 +25,7 @@ function saveText() {
 		 text : escapeText(t.val()) ,
 		 limit : escapeText(l.val()) }
 
-	 if(l.val().match(/^[0-9][0-9][0-9][0-9]\/[0-9][0-9]\/[[0-9][0-9]$/)){
+	 if(l.val().match(/^[0-9][0-9][0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]$/)){
 		 localStorage.setItem(time, JSON.stringify(data));
 		 // テキストボックスを空にする
 		 t.val("");
@@ -36,33 +38,41 @@ function saveText() {
 
 // ローカルストレージに保存した値を再描画する
 function showText() {
-  // すでにある要素を削除する
-  var list = $("#list");
-  list.children().remove();
-  // ローカルストレージに保存された値すべてを要素に追加する
-  var key, value, html = [];
-  for(var i=0, len=localStorage.length; i<len; i++) {
-    key = localStorage.key(i);
-    value = JSON.parse(localStorage.getItem(key));
-    //html.push("<div id=\"list\" class=\"box\">"+"<h2>"+ key+":text " + value.text+" limit "+value.limit+"</h2>"+"</div>");
-	html.push("<div id=\"list\" class=\"box\">
-		 <table>
-			 <tr><td colspan=\"3\">新しいToDoをつくる</td></tr>
-			 <tr><td id=\"right\">ToDo名</td><td>"+ value.limit +"</td><td rowspan="2"> <input id="formButton" type="button" value="ToDoの追加"></td></tr>
-			 <tr><td id="right">期限 </td><td><input id="formLimit" type="text" size="40" maxlength="30" placeholder="yyyy/mm/dd"></td></tr>
-			 <tr><td colspan="3"><div class = "red-bolder" id="warning"></div></td></tr>
-		 </table>
-		 </div>"
-	 );
-  }
-  list.append(html.join(''));
+	 // すでにある要素を削除する
+	 var list = $("#list");
+	 list.children().remove();
+	 // ローカルストレージに保存された値すべてを要素に追加する
+	 var key, value, html = [];
+	 for(var i=0, len=localStorage.length; i<len; i++) {
+		 key = localStorage.key(i);
+		 value = JSON.parse(localStorage.getItem(key));
+		 html.push("<div id=\"list\" class=\"box\">"+
+			 "<table>"+
+				 "<tr><td colspan=\"2\"><p  class=\"bolder\">"+value.text+"</p></td></tr>"+
+				 "<tr><td>期限： </td><td>"+ value.limit +"</td><td rowspan=\"3\"> <input id=\"formButton\" type=\"button\" value=\"未完了\"></td></tr>"+
+				 "<tr><td>作成日： </td><td>"+key+"</td></tr>"+
+			 "</table>"+
+			 "</div>"
+			);
+		}
+	 list.append(html.join(''));
+	 if(localStorage.length > 0){
+		 console.log("true");
+		 return(true);
+		 }
+	 else{
+		 console.log("false");
+		 return(false);
+		 }
 }
-function change_text(){
-//バルス！
-localStorage.clear();
-showText();
- $("#ex").text("ToDoはありません。ToDoを追加してください");
+
+function clear_text(){
+	 //バルス！
+	 localStorage.clear();
+	 showText();
+	 $("#ex").text("ToDoはありません。ToDoを追加してください");
 }
+
 function escapeText(text) {
   return $("<div>").text(text).html();
 }
